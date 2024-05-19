a06893fd7573b1c4266a595b7332c9ea5418194a
genieouz <33516121+genieouz@users.noreply.github.com>
2024-05-19 15:37:57 +0000

    reset pass comonent

---
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.scss'
+})
+export class ResetPasswordComponent {
+  hidePassword: boolean = true;
+}
