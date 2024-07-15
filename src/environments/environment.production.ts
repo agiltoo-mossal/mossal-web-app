22e4f46af2e0bbb0b72d43d5ff7393e589736fb1
genieouz <33516121+genieouz@users.noreply.github.com>
2024-07-15 13:39:28 +0000

    environment setupd

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,8 @@
 export const environment = {
-  GRAPHQL_URI: "http://51.20.69.167:3003/graphql",
-  API_URI: "http://51.20.69.167:3003",
+  // GRAPHQL_URI: "http://51.20.69.167:3003/graphql",
+  // API_URI: "http://51.20.69.167:3003",
+  GRAPHQL_URI: "https://mossall-enterprise-service-9mk2.onrender.com/graphql",
+  API_URI: "https://mossall-enterprise-service-9mk2.onrender.com",
   ENTERPRISE_URI: "http://51.20.69.167:3003",
   KEYCLOAK_URL: "https://ec2-16-171-29-127.eu-north-1.compute.amazonaws.com:8443",
   KEYCLOACK_REALM: "mossall_admins",
