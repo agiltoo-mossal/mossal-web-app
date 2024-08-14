7383ab517cf1d7eeaa52e52111985c498573e226
ratmans <ratma96@gmail.com>
2024-08-14 12:48:41 +0000

    add package XLSX and chage extension to XLSX

--- src/app/shared/components/organization-file/organization-file.component.ts
+++ src/app/shared/components/organization-file/organization-file.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchSupportPaiementGQL } from 'src/graphql/generated';
 import { SnackBarService } from '../../services/snackbar.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-organization-file',
@@ -8,6 +9,10 @@ import { SnackBarService } from '../../services/snackbar.service';
   styleUrl: './organization-file.component.scss',
 })
 export class OrganizationFileComponent implements OnInit {
+  EXCEL_TYPE =
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  EXCEL_EXTENSION = '.xlsx';
+
   constructor(
     private fetchSupportPaiement: FetchSupportPaiementGQL,
     private snackBarService: SnackBarService
@@ -40,16 +45,8 @@ export class OrganizationFileComponent implements OnInit {
               row.amount,
               '',
             ]),
-          ]
-            .map((e) => e.join(','))
-            .join('\n');
-          const blob = new Blob([csvRows], { type: 'text/csv;charset=utf-8;' });
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.setAttribute('href', url);
-          a.setAttribute('download', 'support-paiement.csv');
-          a.click();
-          window.URL.revokeObjectURL(url);
+          ];
+          this.convertToXLSX(csvRows);
         } else {
           this.snackBarService.showSnackBar(
             "Aucune Demande de paiement n'a encore été effectue sur ce mois !"
@@ -58,5 +55,31 @@ export class OrganizationFileComponent implements OnInit {
       },
       error: (error) => console.log(error),
     });
+  }
+
+  convertToXLSX(data: any[]) {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data, {
+      skipHeader: true,
+    });
+    const workbook: XLSX.WorkBook = {
+      Sheets: { data: worksheet },
+      SheetNames: ['data'],
+    };
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+    this.saveAsExcelFile(excelBuffer, 'support-paiement');
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: this.EXCEL_TYPE });
+    const url = window.URL.createObjectURL(data);
+    const a = document.createElement('a');
+    a.setAttribute('href', url);
+    a.setAttribute('download', `${fileName}${this.EXCEL_EXTENSION}`);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    // FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + this.EXCEL_EXTENSION);
   }
 }
