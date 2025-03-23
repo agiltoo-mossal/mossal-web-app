3d0dc9faef31b81792c1ec52bded8794fe07fc6e
ratmans <ratma96@gmail.com>
2025-03-23 20:26:24 +0000

    add some changes on setting duree

--- src/app/shared/components/settings/setting.component.ts
+++ src/app/shared/components/settings/setting.component.ts
@@ -32,7 +32,10 @@ export class SettingComponent implements OnInit {
       activated: [true],
       amountUnit: [AmountUnit.Percentage, [Validators.required]],
       // amount: [],
-      refundDuration: [1, [Validators.required, Validators.min(1)]],
+      refundDuration: [
+        1,
+        [Validators.required, Validators.min(1), Validators.max(7)],
+      ],
       autoValidate: [true],
       amountPercentage: [
         null,
