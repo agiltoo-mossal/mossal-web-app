7c701bf0933d2333b0aa7ad8c2277be87f5d5af6
zizaba <aziz@agiltoo.fr>
2025-07-03 11:32:33 +0000

    update admin list page

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -12,12 +12,6 @@ export const environment = {
   ENTERPRISE_URI:
     'https://mossall-enterprise-service-9mk2.onrender.com/graphql',
 
-  // GRAPHQL_URI: 'http://51.20.69.167:3003/graphql',
-  // API_URI: 'http://51.20.69.167:3003',
-
-  // GRAPHQL_URI: 'http://51.20.69.167:3003/graphql',
-  // API_URI: 'http://51.20.69.167:3003',
-  // ENTERPRISE_URI: 'http://51.20.69.167:3003',
   KEYCLOAK_URL:
     'https://ec2-16-171-29-127.eu-north-1.compute.amazonaws.com:8443',
   KEYCLOACK_REALM: 'mossall_admins',
