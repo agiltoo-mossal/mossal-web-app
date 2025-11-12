bc60d707345aa5104085856710867bc3bcec583c
Jarrabousso <diarra@agiltoo.fr>
2025-11-12 17:52:16 +0000

    change env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  GRAPHQL_URI: 'https://mossall-enterprise-service-preprod.onrender.com/graphql',
-  API_URI: 'https://mossall-enterprise-service-preprod.onrender.com/',
-  ENTERPRISE_URI: 'https://mossall-enterprise-service-preprod.onrender.com/graphql',
+  // GRAPHQL_URI: 'https://mossall-enterprise-service-preprod.onrender.com/graphql',
+  // API_URI: 'https://mossall-enterprise-service-preprod.onrender.com/',
+  // ENTERPRISE_URI: 'https://mossall-enterprise-service-preprod.onrender.com/graphql',
 
   // GRAPHQL_URI: 'http://localhost:3003/graphql',
   // API_URI: 'http://localhost:3003/',
@@ -10,9 +10,9 @@ export const environment = {
   // GRAPHQL_URI: 'http://51.20.69.167:3003/graphql',
   // API_URI: 'http://51.20.69.167:3003',
 
-  // GRAPHQL_URI: 'http://51.20.69.167:3003/graphql',
-  // API_URI: 'http://51.20.69.167:3003',
-  // ENTERPRISE_URI: 'http://51.20.69.167:3003',
+  GRAPHQL_URI: 'https://mossall-ent-preprod.dynv6.net/graphql',
+  API_URI: 'https://mossall-ent-preprod.dynv6.net',
+  ENTERPRISE_URI: 'https://mossall-ent-preprod.dynv6.net',
   KEYCLOAK_URL:
     'https://ec2-16-171-29-127.eu-north-1.compute.amazonaws.com:8443',
   KEYCLOACK_REALM: 'mossall_admins',
