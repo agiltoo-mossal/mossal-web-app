18c830ff8ed526602a907927761ace6d33f2b006
zizaba <aziz@agiltoo.fr>
2026-05-20 09:05:42 +0000

    fix event creation/modification

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,14 @@ export const environment = {
   // API_URI: 'https://mossall-enterprise-service-preprod.onrender.com/',
   // ENTERPRISE_URI: 'https://mossall-enterprise-service-preprod.onrender.com/graphql',
 
-  // GRAPHQL_URI: 'http://localhost:3003/graphql',
-  // API_URI: 'http://localhost:3003/',
-  // ENTERPRISE_URI: 'http://localhost:3003/graphql',
+  GRAPHQL_URI: 'http://localhost:3003/graphql',
+  API_URI: 'http://localhost:3003/',
+  ENTERPRISE_URI: 'http://localhost:3003/graphql',
 
   // New server address
-  GRAPHQL_URI: 'https://mossall-ent-preprod.mossall.com/graphql',
-  API_URI: 'https://mossall-ent-preprod.mossall.com/',
-  ENTERPRISE_URI: 'https://mossall-ent-preprod.mossall.com',
+  // GRAPHQL_URI: 'https://mossall-ent-preprod.mossall.com/graphql',
+  // API_URI: 'https://mossall-ent-preprod.mossall.com/',
+  // ENTERPRISE_URI: 'https://mossall-ent-preprod.mossall.com',
 
 
   // GRAPHQL_URI: 'https://mossall-ent-prod.mossall.com/graphql',
